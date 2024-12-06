937ddf31edb899771165da4ee9b2ba0d231ef343
AbdullahGailani <abdullahgailani004@gmail.com>
2024-12-06 15:48:55 +0300

    Adding JavaScript

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ let typed = new Typed(".auto-type", {
   backSpeed: 150,
   loop: true,
 });
+
+// Sticky Navbar
+
+const heroContainer = document.querySelector(".hero");
+const nav = document.querySelector(".nav");
+
+const stickyNav = (entries) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const navbarObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-10px",
+});
+navbarObserver.observe(heroContainer);
+//Smooth Scroll
+
+const goToBtn = document.querySelectorAll(".go-to");
+
+goToBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    document
+      .querySelector(`#${e.target.value}`)
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
